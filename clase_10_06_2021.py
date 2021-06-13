8e12f96dae873cc8bd99ce1e82d7b57f46f030ec
asepac01 <lorelyangie01@gmail.com>
2021-06-13 00:09:39 -0500

    1 metodo usoDelIf de la clase Condicion.

--- clase_10_06_2021.py
+++ clase_10_06_2021.py
@@ -6,3 +6,11 @@ class Condicion:
         self.numero2 = num2
         # numero = num1 + num2
         self.numero3 = num1
+    
+    def usoDelIf(self):
+        if self.numero1 == self.numero2:
+            print("» El numero 1: {} es igual al numero 2: {}.".format(self.numero1, self.numero2))
+        elif self.numero1 == self.numero3:
+            print("» El numero 1: {} es igual al numero 3: {}.".format(self.numero1, self.numero3))
+        else:
+            print("» No son números iguales.")
